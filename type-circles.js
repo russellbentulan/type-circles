dd38eaa0361c7ee0ac156eb254a77d6ddadf3065
Russell Bentulan <russellbentulan@gmail.com>
2019-10-07 00:14:51 -0400

    added scoring

--- type-circles.js
+++ type-circles.js
@@ -5,11 +5,15 @@ $(function(){
     // Create a play state for the game
     typeCircles.playingGame = null;
 
+    // Log user score
+    typeCircles.currentScore = 0;
+
     // All words (200 should be enough)
     typeCircles.words = ["juno", "surround", "install", "narrow", "constraint", "craftsman", "album", "extent", "carriage", "colon", "landscape", "software", "kettle", "default", "constant", " borrow", "couple", "common", "debut", "absorb", "employ", "basket", "physics", "command", "trouser", "surprise", "doctor", "pioneer", "presence", "handy", "cluster", "decade", "oven", "descent", "publish", "singer", "fossil", "quarrel", "mirror", "balance", "bubble", "promote", "dollar", "sweater", "support", "speaker", "cottage", "powder", "engine", "stubborn", "video", "replace", "reduce", "dairy", "waiter", "accept", "pile", "valid", "final", "ribbon", "favor", "poetry", "scatter", "applaud", "army", "marine", "fraction", "vague", "plastic", "fiction", "mourning", "summer", "construct", "pupil", "suppress", "bathroom", "eaux", "topple", "confine", "export", "feather", "patrol", "nonsense", "standard", "knowledge", "discreet", "pepper", "medal", "lemon", "profound", "proclaim", "leader", "studio", "cabin", "moving", "present", "distort", "discuss", "crystal", "painter", "behave", "entry", "fragrant", "season", "jelly", "discount", "rumor", "bracket", "morning", "earwax", "master", "secure", "latest", "surface", "symbol", "fever", "fixture", "future", "wander", "funny", "freckle", "rebel", "reinforce", "award", "volume", "failure", "apple", "audience", "earthwax", "elapse", "pocket", "navy", "dribble", "piano", "bundle", "expect", "football", "notion", "payment", "spirit", "angle", "castle", "dictate", "sale", "confront", "addicted", "pursuit", "kidnap", "detail", "hover", "thesis", "reverse", "healthy", "relate", "idea", "lesson", "behead", "problem", "applied", "bargain", "echo", "damage", "charter", "perfume", "receipt", "finger", "whole", "portrait", "concern", "ensure", "monster", "social", "rabbit", "even", "warrant", "bury", "journal", "belong", "player", "debate", "hero", "lineage", "sequence", "revoke", "lily", "reward", "sister", "flatware", "feeling", "ethics", "basic", "junior", "control", "agree", "muggy", "looting", "justice", "driver", "linen", "undress"];
 
     typeCircles.word = $('#title');
     typeCircles.answer = $('#user-answer');
+    typeCircles.score = $('#score');
 
     // Pull a random word out of the array
     typeCircles.randomWord = () => {
@@ -19,22 +23,29 @@ $(function(){
     };
     
     // Get a different word
+    // Change the word in the page
     typeCircles.changeWord = () => {
-
+        newWord = typeCircles.randomWord()[0];
+        typeCircles.word.text(newWord);
+        typeCircles.checkAnswer(newWord);
     };
 
-    // Start the game
-    typeCircles.startGame = (theWord) => {
-        console.log(theWord);
+    // Listen to user input
+    typeCircles.checkAnswer = (word) => {
         typeCircles.answer.on('keyup', function() {
-            if (theWord.toUpperCase() === $(this).val().toUpperCase()) {
-            console.log($(this).val());
-        }
+            if (word.toUpperCase() === $(this).val().toUpperCase()) {
+                $(this).val('');
+                typeCircles.word.text('');
+                typeCircles.changeWord();
+            }
         });
     };
 
     // Clear answer input
     typeCircles.answer.val('');
+
+    // Set score
+    typeCircles.score.text(typeCircles.currentScore);
 
     // Hide instructions
     // Change the title to a word
@@ -44,6 +55,6 @@ $(function(){
         typeCircles.word.attr('id', 'word');
         newWord = typeCircles.randomWord()[0];
         typeCircles.word.text(newWord);
-        typeCircles.startGame(newWord);
+        typeCircles.checkAnswer(newWord);
     });
 });
